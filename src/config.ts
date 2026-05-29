b36ac37a72be96049d2149dc46ae764daecfc8aa
max0194 <dolgodonovmaks12@gmail.com>
2026-05-29 10:48:14 +0700

    Changed parameters for tauri

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const isTauri = import.meta.env.VITE_APP_MODE === "tauri";
 const isDev = import.meta.env.DEV;
 
 export const API_URL = isTauri
-  ? "https://api.threatmonitoring.ru"
+  ? "http://192.168.8.138:8085/api"
   : isDev
     ? "/api"
-    : "https://api.threatmonitoring.ru";
+    : "https://api.threatmonitoring.ru/api";
